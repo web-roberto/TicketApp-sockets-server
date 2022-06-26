c8340ff8614396ecc11255f135032ad800cffe7a
robertoweb <roberto.alonso.gandia@gmail.com>
2022-06-26 04:50:02 +0200

    1st commit

---
+++ public/precache-manifest.1450217f43f77fdf8f01dfca91cf03ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b03f861cbfa929d6aa796f37ce8899",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c5143c72887bff4296b",
+    "url": "/static/css/2.dd36e675.chunk.css"
+  },
+  {
+    "revision": "ef1ad053b047c601966f",
+    "url": "/static/css/main.dff3c0e4.chunk.css"
+  },
+  {
+    "revision": "1c5143c72887bff4296b",
+    "url": "/static/js/2.e71cb09d.chunk.js"
+  },
+  {
+    "revision": "68ab86c7c728da17ff6eaefe4ac1bcda",
+    "url": "/static/js/2.e71cb09d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef1ad053b047c601966f",
+    "url": "/static/js/main.4f77631d.chunk.js"
+  },
+  {
+    "revision": "e3a9f56e0be4027bf95a",
+    "url": "/static/js/runtime-main.b56973e4.js"
+  }
+]);
